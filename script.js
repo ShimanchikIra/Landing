6d9b0a4deb24a3c6f6882a2a348cdbde5ffd8efa
ShimanchikIra <shimanchikirina@gmail.com>
2019-06-20 10:55:25 +0300

    add vertical slider

--- script.js
+++ script.js
@@ -1,100 +1,115 @@
-$(document).ready(function(){
+$(document).ready(function() {
     //изменение header по скроллу
     var header = $(".header_top");
     var heightHeader = header.height(); // высота шапки
-    $(window).scroll(function(){
-        if ( $(this).scrollTop() > heightHeader && header.hasClass('header_top') ){
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > heightHeader && header.hasClass('header_top')) {
             header.removeClass('header_top').addClass('header_fixed');
-        } else if($(this).scrollTop() <= heightHeader && header.hasClass('header_fixed')) {
+        } else if ($(this).scrollTop() <= heightHeader && header.hasClass('header_fixed')) {
             header.removeClass("header_fixed").addClass('header_top');
         }
     });//scroll
 
     //отображение меню в header_top по нажатию на кнопку
-    var navButton=$('.nav-button');//кнопка для скрытия/отображения меню в header на первой странице
-    var hideMenu=$('.navigation');//меню, которое необходимо скрытвать и показывать
+    var navButton = $('.nav-button');//кнопка для скрытия/отображения меню в header на первой странице
+    var hideMenu = $('.navigation');//меню, которое необходимо скрытвать и показывать
     navButton.click(function () {
         $(this).toggleClass("nav-button_open");
-        if ($(this).hasClass('nav-button_open')){
-            hideMenu.fadeTo('slow',1);
-        }
-        else {
-            hideMenu.fadeTo('slow',0);
+        if ($(this).hasClass('nav-button_open')) {
+            hideMenu.fadeTo('slow', 1);
+        } else {
+            hideMenu.fadeTo('slow', 0);
         }
     });
 
-    $(".scroll-button").click(function() {
+    //scroll на вторую страницу (по id, id выставлен (переделать на скролл по размеру экрана)
+    $(".scroll-button").click(function () {
         $('html,body').animate({
-                scrollTop: $("#scroll").offset().top},
+                scrollTop: $("#scroll").offset().top
+            },
             'slow');
     });
 
+        // //вертикальный слайдер
+    let itemVerticalSlider = $('.content-vertical-slider-item');//элементы слайдера
+    let heightVerticalSlider = itemVerticalSlider.height();//высота слайдера(то, наскольео нам надо менять расположение слайда для перелистываннния)
+    let itemSwitchVerticalSlider = $('.switch-vertical-slider__item');//элементы переключателя слайдера
+    let timer = 0;
+    console.log(timer);
+    let indVerticalSlider = 0;//счетчик слайдера
+    console.log('pic_index=' + indVerticalSlider);
+    //ставим слайлер  и переключатель в начальное положение
+    itemVerticalSlider.eq(indVerticalSlider).css({
+        top: 0
+    });
+    itemSwitchVerticalSlider.eq(indVerticalSlider).addClass('active');
 
+    startSlider();//запускаем переключение слайдера
+    function startSlider() {
+        if (timer > 0) { return; }
+            timer = setInterval(function () {
+                itemVerticalSlider.eq(indVerticalSlider).stop(true, false).animate({
+                    top: '-' + heightVerticalSlider + 'px',
+                });
+                if (indVerticalSlider < itemVerticalSlider.length - 1) {
+                    indVerticalSlider++;
+                    console.log('pic_index=' + indVerticalSlider);
+                } else {
+                    indVerticalSlider = 0;
+                    console.log('pic_index=' + indVerticalSlider);
+                }
+                itemVerticalSlider.eq(indVerticalSlider).css({
+                    top: heightVerticalSlider + 'px'
+                });
+                itemVerticalSlider.eq(indVerticalSlider).stop(true, false).animate({
+                    top: "0px"
+                });
 
+                itemSwitchVerticalSlider.removeClass('active');
+                itemSwitchVerticalSlider.eq(indVerticalSlider).addClass('active');
 
-    var img=document.getElementsByClassName('vertical-slider__img')[0];//получвем элемент, в который записывается цитата
-    var title=document.getElementsByClassName('vertical-slider__title')[0];//получвем элемент, в который записывается автор
-    var text=$('.vertical-slider__text')[0];
-
-    text.innerHTML=mass[0]['text'];
-    // var n=document.getElementById('numQuotes').children;//получвем массив элемент radio
-
-    img.src = mass[0]['link'];//при загрузке вставляем в HTML первую цитату
-    title.innerHTML=mass[0]['title'];//и ее автора
-    // n[0].checked="checked";// и выбираем первый radio
-    // var i=1;//счетчик для номера цитаты
-    //
-    // //функция, которая отслеживает изменение radio и записывает цитату, номер которой мы выбрали
-    // for(let j=0; j<n.length; j++) {
-    //     n[j].onchange = () => {
-    //         p.innerHTML = l[j]['quote'];
-    //         s.innerHTML=l[j]['author'];
-    //         i=j;
-    //     };
-    // }
-    // //функция, которая меняет цитаты на странице (используется в таймере)
-    // function slider(arr) {
-    //     if (i!==arr.length){
-    //         p.innerHTML = l[i]['quote'];
-    //         s.innerHTML=l[i]['author'];
-    //         n[i].checked="checked";
-    //         return i++;
-    //
-    //     } else {
-    //         p.innerHTML = l[0]['quote'];
-    //         s.innerHTML=l[0]['author'];
-    //         n[0].checked="checked";
-    //         return i=0;
-    //     }
-    //
-    // }
-    //
-    // setInterval(slider, 5000, l);
-
-});
-var mass = [//массив с цитатами
-    {
-        link: "img/benefit1.png",
-        title: "lokk kkkk 1111",
-        text: "2: A new email service being developed by a group from MIT and CERN promises to bring secure,\n" +
-            "encrypted email to the masses and keep sensitive information away from prying eyes.",
-        list: {
-            f: "1",
-            g: "2",
-            y: "3"
+            }, 3000);
+        console.log(timer);
         }
-    },
-    {
-        link: "img/TimelinePC.png",
-        title: "lokk kkkk 22222",
-        text: "2: A new email service being developed by a group from MIT and CERN promises to bring secure,\n" +
-            "encrypted email to the masses and keep sensitive information away from prying eyes."
 
-    },
-    {
-        link: "img/TimelinePC.png",
-        title: "lokk kkkk 3333",
-        text: "2: A new email service being developed by a group from MIT and CERN promises to bring secure,\n" +
-            "encrypted email to the masses and keep sensitive information away from prying eyes."
+    function stopSlider() {
+        clearInterval(timer);
+        timer = 0;
+        console.log(timer);
     }
-];
+
+    $('.switch-vertical-slider').hover(function () {
+        stopSlider();
+        }, function () {
+        startSlider();
+        });
+    itemSwitchVerticalSlider.click(function () {//отслеживваем клики по переключателю
+             if (itemVerticalSlider.length === itemSwitchVerticalSlider.length) {//если количество точек не совпадает с количесвом слайдов, то не даем переключать слайды
+                console.log(indVerticalSlider, $(this).index());
+                if (indVerticalSlider !== $(this).index()) {
+                    stopSlider();
+                    itemVerticalSlider.eq(indVerticalSlider).stop(true, false).animate({
+                        top: '-' + heightVerticalSlider + 'px'
+                    });
+                    console.log('pic_index=' + indVerticalSlider);
+                    indVerticalSlider = $(this).index();
+                    itemVerticalSlider.eq(indVerticalSlider).css({
+                        top: heightVerticalSlider + 'px'
+                    });
+                    itemVerticalSlider.eq(indVerticalSlider).stop(true, false).animate({top: 0});
+                    console.log('pic_index=' + indVerticalSlider);
+                    itemSwitchVerticalSlider.removeClass('active');
+                    $(this).addClass('active');
+
+                    console.log('таймер остановлен');
+                    setTimeout(startSlider, 8000);
+                    console.log('таймер вкд');
+                }
+                 return false;
+            } else {
+                console.log('Невозможно переключать слайдер так как разное количество точек и слайдов');
+            }
+        });
+
+
+    });

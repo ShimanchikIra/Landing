7206030af6d36b6bcf84ad0dfd5f807fa1fd5c7e
ShimanchikIra <shimanchikirina@gmail.com>
2019-06-22 01:08:43 +0300

    add horizontal slider

--- script.js
+++ script.js
@@ -31,7 +31,7 @@ $(document).ready(function() {
             'slow');
     });
 
-        // //вертикальный слайдер
+        // //вертикальный слайдер//переделать через children
     let itemVerticalSlider = $('.content-vertical-slider-item');//элементы слайдера
     let heightVerticalSlider = itemVerticalSlider.height();//высота слайдера(то, наскольео нам надо менять расположение слайда для перелистываннния)
     let itemSwitchVerticalSlider = $('.switch-vertical-slider__item');//элементы переключателя слайдера
@@ -88,7 +88,7 @@ $(document).ready(function() {
              if (itemVerticalSlider.length === itemSwitchVerticalSlider.length) {//если количество точек не совпадает с количесвом слайдов, то не даем переключать слайды
                 console.log(indVerticalSlider, $(this).index());
                 if (indVerticalSlider !== $(this).index()) {
-                    stopSlider();
+                   // stopSlider();
                     itemVerticalSlider.eq(indVerticalSlider).stop(true, false).animate({
                         top: '-' + heightVerticalSlider + 'px'
                     });
@@ -103,7 +103,7 @@ $(document).ready(function() {
                     $(this).addClass('active');
 
                     console.log('таймер остановлен');
-                    setTimeout(startSlider, 8000);
+                   // setTimeout(startSlider, 8000);
                     console.log('таймер вкд');
                 }
                  return false;
@@ -113,4 +113,148 @@ $(document).ready(function() {
         });
 
 
+
+
+// //горизонтальный слайдер
+let itemHorizontalSlider = $('.content-horizontal-slider-item');//элементы слайдера
+let widthHorizontalSlider = itemHorizontalSlider.width();//ширина слайдера(то, наскольео нам надо менять расположение слайда для перелистываннния)
+let itemSwitchHorizontalSlider = $('.switch-horizontal-slider__item');//элементы переключателя слайдера
+let timer2 = 0;
+console.log('width: ' + widthHorizontalSlider);
+let indHorizontalSlider = 0;//счетчик слайдера
+//console.log('pic_index=' + indHorizontalSlider);
+//ставим слайлер  и переключатель в начальное положение
+itemHorizontalSlider.eq(indHorizontalSlider).css({
+    right: 0
+});
+itemSwitchHorizontalSlider.eq(indHorizontalSlider).addClass('active');
+
+
+    itemSwitchHorizontalSlider.click(function () {//отслеживваем клики по переключателю (немного говых учлоыий)
+        if (itemHorizontalSlider.length === itemSwitchHorizontalSlider.length) {//если количество точек не совпадает с количесвом слайдов, то не даем переключать слайды
+            console.log(indHorizontalSlider, $(this).index());
+            if (indHorizontalSlider < $(this).index() ) {
+
+                itemHorizontalSlider.eq(indHorizontalSlider).stop(true, false).animate({
+                    right: '-' + widthHorizontalSlider + 'px'
+                });
+                //console.log('pic_index=' + indVerticalSlider);
+                indHorizontalSlider = $(this).index();
+                itemHorizontalSlider.eq(indHorizontalSlider).css({
+                    right: widthHorizontalSlider + 'px'
+                });
+                itemHorizontalSlider.eq(indHorizontalSlider).stop(true, false).animate({right: 0});
+                //console.log('pic_index=' + indVerticalSlider);
+
+                //console.log('таймер остановлен');
+                //console.log('таймер вкд');
+            } else if (indHorizontalSlider > $(this).index() ) {
+                itemHorizontalSlider.eq(indHorizontalSlider).stop(true, false).animate({
+                    right: widthHorizontalSlider + 'px'
+                });
+                //console.log('pic_index=' + indVerticalSlider);
+                indHorizontalSlider = $(this).index();
+                itemHorizontalSlider.eq(indHorizontalSlider).css({
+                    right: '-' + widthHorizontalSlider + 'px'
+                });
+                itemHorizontalSlider.eq(indHorizontalSlider).stop(true, false).animate({right: 0});
+                //console.log('pic_index=' + indVerticalSlider);
+
+
+            }
+
+            itemSwitchHorizontalSlider.removeClass('active');
+            $(this).addClass('active');
+            // stopSlider2();
+            // setTimeout(startSlider2, 8000);
+            return false;
+        } else {
+            console.log('Невозможно переключать слайдер так как разное количество точек и слайдов');
+        }
     });
+
+    startSlider2();//запускаем переключение слайдера
+    function startSlider2() {
+        if (timer2 > 0) { return; }
+        timer2 = setInterval(function () {
+            itemHorizontalSlider.eq(indHorizontalSlider).stop(true, false).animate({
+                right: '-' + widthHorizontalSlider + 'px'
+            });
+            if (indHorizontalSlider < itemHorizontalSlider.length - 1) {
+                indHorizontalSlider++;
+                //console.log('pic_index=' + indVerticalSlider);
+            } else {
+                indHorizontalSlider = 0;
+            }
+            itemHorizontalSlider.eq(indHorizontalSlider).css({
+                right: widthHorizontalSlider + 'px'
+            });
+            itemHorizontalSlider.eq(indHorizontalSlider).stop(true, false).animate({
+                right: "0px"
+            });
+
+            itemSwitchHorizontalSlider.removeClass('active');
+            itemSwitchHorizontalSlider.eq(indHorizontalSlider).addClass('active');
+
+        }, 3000);
+    }
+
+    function stopSlider2() {
+        clearInterval(timer2);
+        timer2 = 0;
+        console.log(timer2);
+    }
+
+    $('.switch-horizontal-slider').hover(function () {
+        stopSlider2();
+    }, function () {
+        startSlider2();
+    });
+function nextSlide() {
+    itemHorizontalSlider.eq(indHorizontalSlider).stop(true, false).animate({
+        right: '-' + widthHorizontalSlider + 'px'
+    });
+    if (indHorizontalSlider<itemHorizontalSlider.length-1) {
+        indHorizontalSlider++;
+    } else {
+        indHorizontalSlider=0;
+    }
+    //console.log('pic_index=' + indVerticalSlider);
+    itemHorizontalSlider.eq(indHorizontalSlider).css({
+        right: widthHorizontalSlider + 'px'
+    });
+    itemHorizontalSlider.eq(indHorizontalSlider).stop(true, false).animate({right: 0});
+    itemSwitchHorizontalSlider.removeClass('active');
+    itemSwitchHorizontalSlider.eq(indHorizontalSlider).addClass('active');
+
+}
+    $('#next').click(function() {
+        stopSlider2();
+        nextSlide();
+        setTimeout(startSlider2, 8000);
+    });
+    function prevSlide() {
+        itemHorizontalSlider.eq(indHorizontalSlider).stop(true, false).animate({
+            right: widthHorizontalSlider + 'px'
+        });
+        if (indHorizontalSlider>0) {
+            indHorizontalSlider--;
+        } else {
+            indHorizontalSlider=itemHorizontalSlider.length-1;
+        }
+        //console.log('pic_index=' + indVerticalSlider);
+        //indHorizontalSlider = $(this).index();
+        itemHorizontalSlider.eq(indHorizontalSlider).css({
+            right: '-' + widthHorizontalSlider + 'px'
+        });
+        itemHorizontalSlider.eq(indHorizontalSlider).stop(true, false).animate({right: 0});
+        itemSwitchHorizontalSlider.removeClass('active');
+        itemSwitchHorizontalSlider.eq(indHorizontalSlider).addClass('active');
+    }
+    $('#prev').click(function() {
+        stopSlider2();
+        prevSlide();
+        setTimeout(startSlider2, 8000);
+
+    });
+});
